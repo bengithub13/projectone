1a76d94b17f3ac9c9bec7379db60569ac0b53812
BenPoon <ben.poon@gmail.com>
2016-02-06 02:45:27 -0500

    args[1] -file

--- src/edu/gatech/cs6310/projectOne/ProjectOne.java
+++ src/edu/gatech/cs6310/projectOne/ProjectOne.java
@@ -4,8 +4,8 @@ public class ProjectOne {
 
 	public static void main(String[] args) {
 		 StudentScheduler scheduler = new StudentScheduler();
-		 scheduler.calculateSchedule("C:/Users/BenPoon/Desktop/project1/student_demand_600.csv");
-		 
+	//	 scheduler.calculateSchedule("C:/Users/BenPoon/Desktop/project1/student_demand_600.csv");
+		 scheduler.calculateSchedule(args[1]);
 		 
 		 System.out.printf("X=%.2f\n",scheduler.getObjectiveValue());
 		 
